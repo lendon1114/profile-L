f1603b13135aa5a700c152477bf67b1a88b5d2bb
lendon1114 <lendonbracewell1114@gmail.com>
2023-11-24 01:04:09 +0900

    change test

--- components/Home/AboutMe/AboutMe.tsx
+++ components/Home/AboutMe/AboutMe.tsx
@@ -83,7 +83,7 @@ export default function AboutMe(props) {
                 provide excellent{" "}
                 <span className="text-AAsecondary">programming skills,</span>{" "}
                 effective{" "}
-                <span className="text-AAsecondary">scommunication skills</span>{" "}
+                <span className="text-AAsecondary">communication skills</span>{" "}
                 and high quality product.
               </span>
               <br className="md:block hidden" />
